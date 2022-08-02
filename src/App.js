2b955f762fd72a13093af5c241f505dac978a51a
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 02:35:30 +0500

    Create components folder and basic structure in Calculator.js, and render it into App.js

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
+import Calculator from './components/Calculator';
 
 function App() {
   return (
     <div className="App">
       <h1>Math Magicians</h1>
+      <Calculator />
     </div>
   );
 }
---
+++ src/components/Calculator.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+class Calculator extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <h2>Calculator</h2>
+    );
+  }
+}
+
+export default Calculator;

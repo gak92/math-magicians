15f1496de450c47af4fef64605b1c9a9def71463
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 04:29:58 +0500

    Changed App into class-based

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
+/* eslint-disable react/prefer-stateless-function */
+
 import React from 'react';
 import './App.css';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Math Magicians</h1>
-      <Calculator />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Math Magicians</h1>
+        <Calculator />
+      </div>
+    );
+  }
 }
 
 export default App;

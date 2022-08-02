b2b606893b1c57903ac2970e2897d1b80c7a765a
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 04:09:24 +0500

    Remove linters error

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,5 +1,9 @@
-import React from "react";
-import PropTypes from "prop-types";
+/* eslint-disable import/no-extraneous-dependencies */
+/* eslint-disable react/prefer-stateless-function */
+/* eslint-disable no-useless-constructor */
+
+import React from 'react';
+import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   constructor(props) {
@@ -9,13 +13,13 @@ class Button extends React.Component {
   render() {
     const { btnClass, btnValue } = this.props;
     return (
-      <button type='button' className={btnClass}>{btnValue}</button>
+      <button type="button" className={btnClass}>{btnValue}</button>
     );
   }
 }
 
 Button.propTypes = {
-  btnClass: PropTypes.string,
+  btnClass: PropTypes.string.isRequired,
   btnValue: PropTypes.string.isRequired,
 };
 

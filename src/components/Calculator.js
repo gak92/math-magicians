ac688446381adfa9ece8069d55b8f9eb32a17fb9
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 03:44:29 +0500

    Add all remaining buttons

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,4 +1,5 @@
 import React from "react";
+import PropTypes from "prop-types";
 import Button from "./Button";
 
 class Calculator extends React.Component {
@@ -8,8 +9,27 @@ class Calculator extends React.Component {
 
   render() {
     return (
-      <div>
-        <Button btnClass='' btnValue='AC'/>
+      <div className="calc-container">
+        <div className="result-div">0</div>
+        <Button btnClass='btn-calc' btnValue='AC'/>
+        <Button btnClass='btn-calc' btnValue='+/-'/>
+        <Button btnClass='btn-calc' btnValue='%'/>
+        <Button btnClass='btn-calc btn-orange' btnValue='÷'/>
+        <Button btnClass='btn-calc' btnValue='7'/>
+        <Button btnClass='btn-calc' btnValue='8'/>
+        <Button btnClass='btn-calc' btnValue='9'/>
+        <Button btnClass='btn-calc btn-orange' btnValue='x'/>
+        <Button btnClass='btn-calc' btnValue='4'/>
+        <Button btnClass='btn-calc' btnValue='5'/>
+        <Button btnClass='btn-calc' btnValue='6'/>
+        <Button btnClass='btn-calc btn-orange' btnValue='-'/>
+        <Button btnClass='btn-calc' btnValue='1'/>
+        <Button btnClass='btn-calc' btnValue='2'/>
+        <Button btnClass='btn-calc' btnValue='3'/>
+        <Button btnClass='btn-calc btn-orange' btnValue='+'/>
+        <Button btnClass='btn-calc btn-zero' btnValue='0'/>
+        <Button btnClass='btn-calc' btnValue='.'/>
+        <Button btnClass='btn-calc btn-orange' btnValue='='/>
       </div>
     );
   }

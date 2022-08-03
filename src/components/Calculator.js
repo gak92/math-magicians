18986a92a5caf5b30d5e6f52c701eba95b7e68fe
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 19:22:04 +0500

    Remove linters error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -25,33 +25,38 @@ class Calculator extends React.Component {
     const { total, next, operation } = this.state;
     const output = `${total || ''}${operation || ''}${next || ''}`;
     const btns = [
-      {class:'btn-calc', value:'AC'},
-      {class:'btn-calc', value:'+/-'},
-      {class:'btn-calc', value:'%'},
-      {class:'btn-calc btn-orange', value:'÷'},
-      {class:'btn-calc', value:'7'},
-      {class:'btn-calc', value:'8'},
-      {class:'btn-calc', value:'9'},
-      {class:'btn-calc btn-orange', value:'x'},
-      {class:'btn-calc', value:'4'},
-      {class:'btn-calc', value:'5'},
-      {class:'btn-calc', value:'6'},
-      {class:'btn-calc btn-orange', value:'-'},
-      {class:'btn-calc', value:'1'},
-      {class:'btn-calc', value:'2'},
-      {class:'btn-calc', value:'3'},
-      {class:'btn-calc btn-orange', value:'+'},
-      {class:'btn-calc btn-zero', value:'0'},
-      {class:'btn-calc', value:'.'},
-      {class:'btn-calc btn-orange', value:'='},
+      { class: 'btn-calc', value: 'AC' },
+      { class: 'btn-calc', value: '+/-' },
+      { class: 'btn-calc', value: '%' },
+      { class: 'btn-calc btn-orange', value: '÷' },
+      { class: 'btn-calc', value: '7' },
+      { class: 'btn-calc', value: '8' },
+      { class: 'btn-calc', value: '9' },
+      { class: 'btn-calc btn-orange', value: 'x' },
+      { class: 'btn-calc', value: '4' },
+      { class: 'btn-calc', value: '5' },
+      { class: 'btn-calc', value: '6' },
+      { class: 'btn-calc btn-orange', value: '-' },
+      { class: 'btn-calc', value: '1' },
+      { class: 'btn-calc', value: '2' },
+      { class: 'btn-calc', value: '3' },
+      { class: 'btn-calc btn-orange', value: '+' },
+      { class: 'btn-calc btn-zero', value: '0' },
+      { class: 'btn-calc', value: '.' },
+      { class: 'btn-calc btn-orange', value: '=' },
     ];
-    
+
     return (
       <div className="calc-container">
         <div className="result-div">{output}</div>
         {
           btns.map((btn) => (
-            <Button btnClass={btn.class} btnClickHandler={this.calculateOutput} btnValue={btn.value} key={btn.value}/>
+            <Button
+              btnClass={btn.class}
+              btnClickHandler={this.calculateOutput}
+              btnValue={btn.value}
+              key={btn.value}
+            />
           ))
         }
       </div>

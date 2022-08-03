f490cb07696677b7f847b9c5e21c4776828fec53
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 18:07:33 +0500

    Update button styling and remove linters error

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -22,30 +22,30 @@ class Calculator extends React.Component {
   }
 
   render() {
-    const {total, next, operation} = this.state;
+    const { total, next, operation } = this.state;
     const output = `${total || ''}${operation || ''}${next || ''}`;
     return (
       <div className="calc-container">
         <div className="result-div">{output}</div>
-        <Button btnClass="btn-calc" btnValue="AC" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="+/-" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="%" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-orange" btnValue="÷" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="7" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="8" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="9" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-orange" btnValue="x" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="4" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="5" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="6" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-orange" btnValue="-" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="1" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="2" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="3" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-orange" btnValue="+" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-zero" btnValue="0" btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc" btnValue="." btnClickHandler={this.calculateOutput}/>
-        <Button btnClass="btn-calc btn-orange" btnValue="=" btnClickHandler={this.calculateOutput}/>
+        <Button btnClass="btn-calc" btnValue="AC" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="+/-" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="%" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-orange" btnValue="÷" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="7" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="8" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="9" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-orange" btnValue="x" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="4" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="5" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="6" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-orange" btnValue="-" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="1" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="2" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="3" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-orange" btnValue="+" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-zero" btnValue="0" btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc" btnValue="." btnClickHandler={this.calculateOutput} />
+        <Button btnClass="btn-calc btn-orange" btnValue="=" btnClickHandler={this.calculateOutput} />
       </div>
     );
   }

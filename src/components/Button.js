d0c8d1adf61bb2a59b3201d363a1232646c5f03f
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-03 13:17:58 +0500

    Remove linters error, install prop-types, and add empty state into each class constructor

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,11 @@
-/* eslint-disable import/no-extraneous-dependencies */
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
-
 import React from 'react';
 import PropTypes from 'prop-types';
 
 class Button extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
   }
 
   render() {
--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,12 +1,11 @@
-/* eslint-disable react/prefer-stateless-function */
-/* eslint-disable no-useless-constructor */
-
 import React from 'react';
 import Button from './Button';
 
 class Calculator extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {};
   }
 
   render() {

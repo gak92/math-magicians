643a5230acb8770a75bf62e6557eef77b8eb7773
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-10 02:49:49 +0500

    Add styling for the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   return (
-    <div>
+    <div className="home-main">
       <h2>Welcome to our page</h2>
       <p>
       "Math magicians" is a website for all fans of mathematics. It is a Single Page App (SPA) that allows users to:

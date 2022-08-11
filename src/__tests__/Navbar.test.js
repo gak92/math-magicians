89bcc381ca5cab60c4d4503c83f2f70f7d22f22a
gak92 <44802922+gak92@users.noreply.github.com>
2022-08-11 13:10:44 +0500

    Remove linters error

--- src/__tests__/Navbar.test.js
+++ src/__tests__/Navbar.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
 import { BrowserRouter } from 'react-router-dom';
-import Navbar from '../components/NavBar';
+import Navbar from '../components/Navbar';
 
 test('NavBar renders accurately', () => {
   render(
